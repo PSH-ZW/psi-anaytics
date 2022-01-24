b80933d56bec5f78a1b1eff89391088a3d31af00
Vishnu <vishnu@nuchange.com>
2022-01-24 10:19:38 +0530

    Code cleanup and whitespace changes

--- src/main/resources/liquibase/scripts/run_liquibase.sh
+++ src/main/resources/liquibase/scripts/run_liquibase.sh
@@ -7,7 +7,7 @@ CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dli
 LIQUIBASE_JAR="${BASEDIR}/../lib/liquibase-core-3.5.3.jar"
 echo "liquibase jar"
 DRIVER="org.postgresql.Driver"
-CREDS="--url=jdbc:postgresql://localhost:5432/analytics --username=analytics --password=password"
+CREDS="--url=jdbc:postgresql://localhost:5432/analytics --username=postgres --password=password"
 CHANGE_LOG_FILE="${BASEDIR}/../changesets/liquibase.xml"
 
 java $CHANGE_LOG_TABLE  -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$CLASSPATH --logLevel=debug --changeLogFile=$CHANGE_LOG_FILE $CREDS update
