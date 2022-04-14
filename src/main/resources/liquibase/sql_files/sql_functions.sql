242da6b9d1dd6da7be8d390796991530498531a5
Vishnu <vishnu@nuchange.com>
2022-04-14 17:54:37 +0530

    Update get_concept_name function to return short_name of concept first instead of fully specified name

--- src/main/resources/liquibase/sql_files/sql_functions.sql
+++ src/main/resources/liquibase/sql_files/sql_functions.sql
@@ -5,8 +5,8 @@ RETURNS varchar(255)
 DETERMINISTIC
 BEGIN
    DECLARE con_name varchar(255);
-   SELECT name into con_name from concept_name where concept_id =  cast(concept_id_str as UNSIGNED)
-   and concept_name_type = 'FULLY_SPECIFIED' and locale = 'en';
+   SELECT coalesce((select name from concept_name where concept_id =  cast(concept_id_str as UNSIGNED) and concept_name_type = 'SHORT' and locale = 'en'),
+       (select name from concept_name where concept_id =  cast(concept_id_str as UNSIGNED) and concept_name_type = 'FULLY_SPECIFIED' and locale = 'en')) into con_name;
    IF con_name = 'Yes' THEN
        SET con_name = 'true';
    ELSEIF con_name = 'No' THEN
